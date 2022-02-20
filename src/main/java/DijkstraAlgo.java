abed1764ee88a77eebed9b0bf75ae12825903ac4
anna <luribina@gmail.com>
2022-02-20 18:28:09 +0300

    Add dijkstra algorithm implementation and tests for it

---
+++ src/main/java/DijkstraAlgo.java
@@ -0,0 +1,96 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class IntPair {
+    int left, right;
+
+    public IntPair(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class DijkstraOutput {
+    int[] costs;
+    int[] paths;
+    int[] visited;
+
+    public DijkstraOutput(int[] costs, int[] paths, int[] visited) {
+        this.costs = costs;
+        this.paths = paths;
+        this.visited = visited;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DijkstraOutput that = (DijkstraOutput) o;
+        return Arrays.equals(costs, that.costs) && Arrays.equals(paths, that.paths) && Arrays.equals(visited, that.visited);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(costs);
+        result = 31 * result + Arrays.hashCode(paths);
+        result = 31 * result + Arrays.hashCode(visited);
+        return result;
+    }
+}
+
+public class DijkstraAlgo {
+    public static List<List<IntPair>> adjacencyMatrixToList(int[][] matrix) {
+        int size = matrix.length;
+        ArrayList<List<IntPair>> graph = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            graph.add(new ArrayList<>());
+            for (int j = 0; j < size; j++) {
+                if (matrix[i][j] != 0) {
+                    graph.get(i).add(new IntPair(j, matrix[i][j]));
+                }
+            }
+        }
+        return graph;
+    }
+
+    public static DijkstraOutput findPaths(List<List<IntPair>> graph, int start) {
+        ArrayList<Integer> paths = new ArrayList<>();
+        int size = graph.size();
+        int[] d = new int[size],
+                p = new int[size];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        Arrays.fill(p, -1);
+        d[start] = 0;
+
+        boolean[] u = new boolean[size];
+        Arrays.fill(u, false);
+
+        for (int i = 0; i < size; i++) {
+            int v = -1;
+            for (int j = 0; j < size; j++) {
+                if (!u[j] && (v == -1 || d[j] < d[v])) {
+                    v = j;
+                    paths.add(v);
+                }
+            }
+            if (d[v] == Integer.MAX_VALUE) {
+                paths.add(-1);
+                break;
+            }
+            u[v] = true;
+
+            for (int j = 0; j < graph.get(v).size(); j++) {
+                int to = graph.get(v).get(j).left,
+                        len = graph.get(v).get(j).right;
+                if (d[v] + len < d[to]) {
+                    d[to] = d[v] + len;
+                    p[to] = v;
+                    paths.add(to);
+                }
+            }
+        }
+        return new DijkstraOutput(d, p, paths.stream().mapToInt(i -> i).toArray());
+    }
+}
+
---
+++ src/test/java/DijkstraAlgoTests.java
@@ -0,0 +1,118 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+public class DijkstraAlgoTests {
+
+    private static Stream<Arguments> undirectedGraph() {
+        int[][] matrix = {
+                {0, 9, 9, 6, 7, 0, 0, 0},
+                {9, 0, 2, 6, 0, 7, 0, 0},
+                {9, 2, 0, 0, 0, 0, 5, 0},
+                {6, 6, 0, 0, 0, 0, 0, 0},
+                {7, 0, 0, 0, 0, 0, 4, 1},
+                {0, 7, 0, 0, 0, 0, 2, 0},
+                {0, 0, 5, 0, 4, 2, 0, 4},
+                {0, 0, 0, 0, 1, 0, 4, 0}
+        };
+        return Stream.of(
+                Arguments.of(matrix, 2, new DijkstraOutput(
+                        new int[]{9, 2, 0, 8, 9, 7, 5, 9},
+                        new int[]{2, 2, -1, 1, 6, 6, 2, 6},
+                        new int[]{0, 2, 0, 1, 6, 0, 1, 3, 5, 0, 3, 6, 4, 5, 7, 0, 3, 5, 0, 3, 0, 4, 7}
+                )),
+                Arguments.of(matrix, 5, new DijkstraOutput(
+                        new int[]{13, 7, 7, 13, 6, 0, 2, 6},
+                        new int[]{4, 5, 6, 1, 6, -1, 5, 6},
+                        new int[]{0, 5, 1, 6, 0, 1, 6, 2, 4, 7, 0, 1, 4, 0, 0, 1, 7, 0, 1, 3, 0, 2, 0, 3}
+                ))
+        );
+    }
+
+    private static Stream<Arguments> directedGraph() {
+        int[][] matrix = {
+                {0, 2, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 4, 1, 0},
+                {0, 9, 0, 0, 2, 0, 3, 0},
+                {0, 5, 0, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 0, 0, 9, 1},
+                {0, 5, 0, 1, 0, 0, 6, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 2, 0}
+        };
+        return Stream.of(
+                Arguments.of(matrix, 1, new DijkstraOutput(
+                        new int[]{Integer.MAX_VALUE, 0, Integer.MAX_VALUE, 5, Integer.MAX_VALUE, 4, 1, Integer.MAX_VALUE},
+                        new int[]{-1, -1, -1, 5, -1, 1, 1, -1},
+                        new int[]{0, 1, 5, 6, 0, 5, 6, 0, 5, 3, 0, 3, 0, -1}
+                )),
+                Arguments.of(matrix, 6, new DijkstraOutput(
+                        new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE},
+                        new int[]{-1, -1, -1, -1, -1, -1, -1, -1},
+                        new int[]{0, 6, 0, -1}
+                ))
+        );
+    }
+
+    private static Stream<Arguments> emptyGraph() {
+        int[][] matrix = {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+        return Stream.of(
+                Arguments.of(matrix, 0, new DijkstraOutput(
+                        new int[]{0, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                        new int[]{-1, -1, -1, -1, -1, -1, -1, -1},
+                        new int[]{0, 1, -1}
+                )),
+                Arguments.of(matrix, 5, new DijkstraOutput(
+                        new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 0, Integer.MAX_VALUE, Integer.MAX_VALUE},
+                        new int[]{-1, -1, -1, -1, -1, -1, -1, -1},
+                        new int[]{0, 5, 0, -1}
+                ))
+        );
+    }
+
+    private static Stream<Arguments> invalidIndex() {
+        return Stream.of(
+                Arguments.of(new int[][]{
+                        new int[] {0,0},
+                        new int[] {0,0}
+                }, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("undirectedGraph")
+    void testUndirectedGraph(int[][] matrix, int start, DijkstraOutput dijkstraOutput) {
+        assertEquals(dijkstraOutput, DijkstraAlgo.findPaths(DijkstraAlgo.adjacencyMatrixToList(matrix), start));
+    }
+
+    @ParameterizedTest
+    @MethodSource("directedGraph")
+    void testDirectedGraph(int[][] matrix, int start, DijkstraOutput dijkstraOutput) {
+        assertEquals(dijkstraOutput, DijkstraAlgo.findPaths(DijkstraAlgo.adjacencyMatrixToList(matrix), start));
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyGraph")
+    void testEmptyGraph(int[][] matrix, int start, DijkstraOutput dijkstraOutput) {
+        assertEquals(dijkstraOutput, DijkstraAlgo.findPaths(DijkstraAlgo.adjacencyMatrixToList(matrix), start));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidIndex")
+    void testInvalidIndex(int[][] matrix, int start) {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> DijkstraAlgo.findPaths(DijkstraAlgo.adjacencyMatrixToList(matrix), start));
+    }
+}
